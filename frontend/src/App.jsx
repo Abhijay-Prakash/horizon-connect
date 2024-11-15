fd95e1ae4144b9f923966242da135177922224e6
Abhijay-Prakash <Abhijay-Prakash>
2024-11-15 09:21:03 +0530

    react routing done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,18 @@ import './index.css'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/Signup'
 import Home from './pages/home/Home'
+import { Route, Routes } from 'react-router-dom'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return <div className='p-4 h-screen flex items-center justify-center'>
-    <Home />
-    {/*<SignUp />*/}
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
   </div>
 
 }
